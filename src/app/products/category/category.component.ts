f5a3641d1ca127420962ec6ef237f6ee1517d59c
vipomage <vipomage@gmail.com>
2019-01-17 16:36:33 +0200

    category component added

---
+++ src/app/products/category/category.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css'],
+})
+export class CategoryComponent implements OnInit {
+  @Input() category;
+  @Input() collection;
+  constructor() {}
+
+  ngOnInit() {}
+}
