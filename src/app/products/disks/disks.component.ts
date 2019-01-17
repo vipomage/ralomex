2d96b404d34cdd02a2687b5bdc1b68a1464c5770
vipomage <vipomage@gmail.com>
2019-01-17 16:36:57 +0200

    disks component update

--- src/app/products/disks/disks.component.ts
+++ src/app/products/disks/disks.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PloughCategory } from '../../interfaces/plough-category';
+import { FireService } from '../../fire.service';
 
 @Component({
   selector: 'app-disks',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisksComponent implements OnInit {
 
-  constructor() { }
+  data:PloughCategory;
+  categories:String[];
+
+  constructor(private db:FireService) { }
 
   ngOnInit() {
+
+    this.db.getCategory('disks').subscribe(response => {
+      this.data = response;
+      this.categories = Object.keys(response);
+    });
+
     history.replaceState('', '', 'products/disks');
   }
 
