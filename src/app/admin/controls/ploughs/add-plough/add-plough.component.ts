e8b823ecf42f7bef4cfd8d6ec76c595bbf476d03
vipomage <vipomage@gmail.com>
2019-01-19 20:28:19 +0200

    add-plough component

--- src/app/admin/controls/ploughs/add-plough/add-plough.component.ts
+++ src/app/admin/controls/ploughs/add-plough/add-plough.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FireService } from '../../../../fire.service';
+import { Plough } from '../../../../interfaces/plough';
 
 @Component({
   selector: 'app-add-plough',
   templateUrl: './add-plough.component.html',
-  styleUrls: ['./add-plough.component.css']
+  styleUrls: ['./add-plough.component.css'],
 })
 export class AddPloughComponent implements OnInit {
-  categories:String[];
-  category: any;
-  constructor(private db:FireService) { }
+
+  categories: String[];
+  category: String;
+  plough: Plough;
+
+  constructor(private db: FireService) {}
+
+  onCategoryChange = selectValue => (this.category = selectValue);
+
+  savePlough = (data: Plough, category: String) =>
+    this.db.addPlough(data, category);
 
   ngOnInit() {
-    this.db.getCategory('ploughs').subscribe(data=>{
-      this.categories = Object.keys(data);
-    })
-  }
 
+    this.db.getCategory('ploughs').subscribe(data => {
+      this.category = Object.keys(data)[0];
+      this.categories = Object.keys(data);
+    });
+  }
 }
