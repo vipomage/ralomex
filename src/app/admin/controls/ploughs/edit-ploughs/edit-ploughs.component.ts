a70674c5cbcbe08c004fb8c1e2e4019ecf40332c
vipomage <vipomage@gmail.com>
2019-01-19 20:28:41 +0200

    edit-plough component

--- src/app/admin/controls/ploughs/edit-ploughs/edit-ploughs.component.ts
+++ src/app/admin/controls/ploughs/edit-ploughs/edit-ploughs.component.ts
@@ -7,12 +7,26 @@ import { FireService } from '../../../../fire.service';
   styleUrls: ['./edit-ploughs.component.css']
 })
 export class EditPloughsComponent implements OnInit {
+  category:String;
   categories:String[];
+  data;
   constructor(private db:FireService) { }
+
+
+  onCategoryChange = (selectValue:String) => {
+    this.category = selectValue;
+
+  };
+
 
   ngOnInit() {
     this.db.getCategory('ploughs').subscribe(data=>{
-      this.categories = Object.keys(data)
+      this.category = Object.keys(data)[0];
+      this.categories = Object.keys(data);
+      this.db.getCategory(this.category).subscribe(res=>{
+        this.data = res;
+        console.log(this.data);
+      })
     })
   }
 
