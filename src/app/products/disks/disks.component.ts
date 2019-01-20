fa7a0d2ae8b401398c907f257a30cc421a287660
vipomage <vipomage@gmail.com>
2019-01-20 12:01:22 +0200

    disks component (sample)

--- src/app/products/disks/disks.component.ts
+++ src/app/products/disks/disks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PloughCategory } from '../../interfaces/plough-category';
-import { FireService } from '../../fire.service';
+import { PloughCategory } from '../../../tools/interfaces/plough-category';
+import { FireService } from '../../../tools/services/fire.service';
 
 @Component({
   selector: 'app-disks',
