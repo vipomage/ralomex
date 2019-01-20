80384aa2def7fab0084ac60cf9be2c0ad667775b
vipomage <vipomage@gmail.com>
2019-01-20 11:58:47 +0200

    admin/material table (temp)

---
+++ src/app/admin/mdb-table/mdb-table.component.ts
@@ -0,0 +1,53 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
+import { FireService } from '../../../tools/services/fire.service';
+
+
+@Component({
+  selector: 'app-mdb-table',
+  templateUrl: './mdb-table.component.html',
+  styleUrls: ['./mdb-table.component.css']
+})
+export class MdbTableComponent implements OnInit {
+  @ViewChild(DataTableDirective)
+  datatableElement: DataTableDirective;
+  min: number;
+  max: number;
+  @Input() category;
+  @Input() data:any;
+
+  dtOptions: DataTables.Settings = {
+    autoWidth:true
+  };
+
+  constructor(private db:FireService) { }
+
+  ngOnInit() {
+
+    $(document).ready(function (){
+        $('#dtBasicExample').DataTable({
+          autoWidth:true,
+          ordering:true
+        });
+        $('.dataTables_length').addClass('bs-select')
+    })
+  }
+
+  editItem =(key:String) => this.db.editItem(key,this.category).subscribe(res=>{
+    console.log(res);
+  });
+
+  ngOnDestroy(): void {
+    // We remove the last function in the global ext search array so we do not add the fn each time the component is drawn
+    // /!\ This is not the ideal solution as other components may add other search function in this array, so be careful when
+    // handling this global variable
+    $.fn['dataTable'].ext.search.pop();
+  }
+
+  filterById(): void {
+    this.datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      dtInstance.draw();
+    });
+  }
+
+}
