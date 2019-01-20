b9855f73f72ec5858dba00217f90b6cbbfb6c7d3
vipomage <vipomage@gmail.com>
2019-01-20 11:59:03 +0200

    admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
+import { AuthService } from '../../tools/services/auth.service';
 
 @Component({
   selector: 'app-admin',
