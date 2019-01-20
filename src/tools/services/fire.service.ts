3bccf220d1fea84244d42858b4e5a9e1dd7fddbc
vipomage <vipomage@gmail.com>
2019-01-20 12:08:26 +0200

    fire service update

--- src/tools/services/fire.service.ts
+++ src/tools/services/fire.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { PloughCategory } from './interfaces/plough-category';
-import { Plough } from './interfaces/plough';
-import { environment } from '../environments/environment';
+import { PloughCategory } from '../interfaces/plough-category';
+import { Plough } from '../interfaces/plough';
+import { environment } from '../../environments/environment';
 import { AuthService } from './auth.service';
 
 const databaseUrl: String = environment.firebase.databaseURL;
@@ -22,16 +22,19 @@ export class FireService {
   addPlough = (data: Plough, category: String) =>
     this.db.database.ref(`ploughs/${category}/collection`).push(data);
 
-  addPloughCategory = (category, categoryDetails) =>
+  addPloughCategory = (category, categoryDetails:PloughCategory) =>
     this.db.database.ref(`ploughs/${category}`).set({
       collection: [],
       description: categoryDetails.description,
-      image: categoryDetails.image,
+      images: categoryDetails.images,
       name: categoryDetails.name,
     });
 
   getCategory = (category): Observable<PloughCategory> =>
     this.http.get<PloughCategory>(`${databaseUrl}/${category}.json`);
+
+  editItem = (key:String,category:String)=>
+    this.http.get<Plough>(`${databaseUrl}/ploughs/${category}/collection/${key}.json`)
 
   removePlough = (id: String) =>
     this.db.database.ref(`ploughs/${id}`).remove(e => console.log(e.message));
