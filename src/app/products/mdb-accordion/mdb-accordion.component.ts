6c8a80730b9222ab7e367e7febe2b0034771c7f9
vipomage <vipomage@gmail.com>
2019-02-04 19:08:06 +0200

    mdb-accordion revamp

--- src/app/products/mdb-accordion/mdb-accordion.component.ts
+++ src/app/products/mdb-accordion/mdb-accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mdb-accordion',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MdbAccordionComponent implements OnInit {
   multiple:boolean = true;
+  @Input() data:any;
+  @Input() category:any;
+  subCategories:String[];
+
+
   constructor() { }
 
+  logging = ()=>{
+    console.log('Works!');
+  }
+
+
   ngOnInit() {
+    this.subCategories = Object.keys(this.data.collection);
+    console.log(this.subCategories);
   }
 
 }
