b49bc2a3912faae9fbc8036a4ebb56a50ca6dfa7
vipomage <vipomage@gmail.com>
2019-02-06 19:00:53 +0200

    product-table component logic

--- src/app/products/product-table/product-table.component.ts
+++ src/app/products/product-table/product-table.component.ts
@@ -1,15 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-table',
   templateUrl: './product-table.component.html',
-  styleUrls: ['./product-table.component.css']
+  styleUrls: ['./product-table.component.css'],
 })
 export class ProductTableComponent implements OnInit {
   @Input() data;
-  constructor() { }
+
+  private type:String;
+  private category:String;
+  private subCategory:String;
+
+  constructor(private route: ActivatedRoute) {}
+
 
   ngOnInit() {
+    this.route.params.subscribe(data => console.log(data));
+    this.type = this.route.snapshot.params.type;
+    this.category = this.route.snapshot.params.category;
+    this.subCategory = this.route.snapshot.params.subCategory;
   }
-
 }
