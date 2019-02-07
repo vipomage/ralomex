de8568ab5d61f20ddb110ae5788f55edfb8192b5
vipomage <vipomage@gmail.com>
2019-02-07 09:14:37 +0200

    home component rebuild

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FireService } from '../../tools/services/fire.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public homeProducts;
+
+  constructor(private db: FireService,private router:Router) {}
+
 
   ngOnInit() {
+   this.db
+      .getHomeProducts()
+      .subscribe(res=>this.homeProducts = res);
   }
 
 }
