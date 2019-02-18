a1a23cbb0fab17be1ea91f1664cbd812c9d62b39
vipomage <vipomage@gmail.com>
2019-02-18 06:22:34 +0200

    update on fire service to reflect DB Change added new functionality- addSubCategory, AddPloughCategory, AddPlough(update) ditched PloughCategory interface for now

--- src/tools/services/fire.service.ts
+++ src/tools/services/fire.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { HttpClient } from '@angular/common/http';
-import { PloughCategory } from '../interfaces/plough-category';
 import { Plough } from '../interfaces/plough';
 import { environment } from '../../environments/environment';
 import { AuthService } from './auth.service';
@@ -20,37 +19,40 @@ export class FireService {
 
   getHomeProducts = () => this.http.get(`${databaseUrl}/homeProducts.json`);
 
-  getItem = (
-    type: String,
-    category: String,
-    subCategory: String,
-    id: String
-  ) => {
-    return this.http.get(
-      `${databaseUrl}/${type}/types/${category}/collection/${subCategory}/collection/`.toLocaleLowerCase() +
-        `${id}.json`
-    );
-  };
+  getItem = ( type: String, category: String, subCategory: String,id: String) =>
+    this.http.get(`${databaseUrl}/${type}/types/${category}/collection/${subCategory}/collection/`.toLocaleLowerCase()+`${id}.json`);
+
 
   getType = (type: String) => this.http.get(`${databaseUrl}/${type}.json`);
+
+  getSubCategories = (type, category) => {
+    return this.http.get(
+      `${databaseUrl}/${type}/types/${category}/collection.json`
+    );
+  };
 
   getSubCategoryData = (type, category, subCategory) =>
     this.http.get(
       `${databaseUrl}/${type}/types/${category}/collection/${subCategory}/collection.json`.toLocaleLowerCase()
     );
 
-  addPlough = (data: Plough, category: String) =>
-    this.db.database.ref(`ploughs/${category}/collection`).push(data);
+  addPlough = (data: Plough, category: String, subCategory: String) =>
+    this.db.database
+      .ref(`ploughs/types/${category}/collection/${subCategory}/collection`)
+      .push(data);
 
-  addPloughCategory = (category, categoryDetails: PloughCategory) =>
-    this.db.database.ref(`ploughs/types/${category.toLowerCase()}`).set({
-      collection: [],
-      description: categoryDetails.description,
-      images: categoryDetails.images,
-      name: categoryDetails.name,
-    });
-
-  addSubCategory = (generalCategory: String) => {};
+  addPloughCategory = (type,categoryDetails )=> {
+    return this.db.database
+      .ref(
+        `ploughs/types/${type}/collection/${categoryDetails.name.toLowerCase()}`
+      )
+      .set({
+        collection: [],
+        description: categoryDetails.description,
+        image: categoryDetails.image,
+        name: categoryDetails.name,
+      });
+  };
 
   editItem = (key: String, category: String) =>
     this.http.get<Plough>(
