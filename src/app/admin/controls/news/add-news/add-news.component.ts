9e9826872aee9a49381ebb47caa40729df775135
vipomage <vipomage@gmail.com>
2019-02-20 02:42:44 +0200

    add news component update edit news component update news control update remove-news component Deprecated

--- src/app/admin/controls/news/add-news/add-news.component.ts
+++ src/app/admin/controls/news/add-news/add-news.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ImageService} from "../../../../../tools/services/image.service";
+import {News} from "../../../../../tools/interfaces/news";
+import {FireService} from "../../../../../tools/services/fire.service";
 
 @Component({
   selector: 'app-add-news',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNewsComponent implements OnInit {
 
-  constructor() { }
+  image: String|String[];
+
+  constructor(
+    private imgService:ImageService,
+    private db:FireService
+    ) { }
+
+  startUpload = (files) =>{
+    try {
+      this.imgService.startUpload(files);
+      this.image = this.imgService.images
+    }catch (e) {
+      console.log(e.message);
+      this.imgService.preventEdit = false;
+    }
+  };
+
+  saveNewsElement = (formData:News)=>{
+    formData.timeStamp = Date.now();
+    formData.image = this.imgService.images;
+    this.db.NewsControls.addNewsElement(formData);
+  };
 
   ngOnInit() {
+
   }
 
 }
--- src/app/admin/controls/news/edit-news/edit-news.component.ts
+++ src/app/admin/controls/news/edit-news/edit-news.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { News } from '../../../../../tools/interfaces/news';
+import { FireService } from '../../../../../tools/services/fire.service';
+import {ImageService} from "../../../../../tools/services/image.service";
 
 @Component({
   selector: 'app-edit-news',
   templateUrl: './edit-news.component.html',
-  styleUrls: ['./edit-news.component.css']
+  styleUrls: ['./edit-news.component.css'],
 })
 export class EditNewsComponent implements OnInit {
+  elements: News[];
+  newsElementId: String;
+  newsElement: News;
 
-  constructor() { }
+  constructor(private db: FireService,private imgService:ImageService) {}
+
+  setElement = (id: String, element: News): void => {
+    this.newsElementId = id;
+    this.newsElement = element;
+  };
+
+  deleteElement = (elementId: string) => {
+    let confirm = window.confirm('Сигурни ли сте че искате да изтриете тази новина!');
+    if (confirm) {
+      window.document.getElementById(elementId).remove();
+      return this.db.NewsControls.deleteNewsElement(elementId);
+    }
+  };
+
+  startUpload = (files) =>{
+    try {
+      this.imgService.startUpload(files);
+      this.newsElement.image = this.imgService.images
+    }catch (e) {
+      console.log(e.message);
+      this.imgService.preventEdit = false;
+    }
+  };
+
+  editNewsElement = (id: String, formData: News) => {
+    if (this.imgService.images.length>0) {
+      formData.image = this.imgService.images[0];
+    }else{
+      formData.image = this.newsElement.image
+    }
+    return this.db.NewsControls.updateNewsElement(id, formData);
+  };
 
   ngOnInit() {
+    this.db.NewsControls.getNewsElements().subscribe((data: News[]) => (this.elements = data));
   }
-
 }
