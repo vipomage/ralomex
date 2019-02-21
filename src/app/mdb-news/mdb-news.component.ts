23045e616952ad55d08673480f7d2d037b4d5e5c
vipomage <vipomage@gmail.com>
2019-02-21 05:29:26 +0200

    mdb-news component update to read from DB and display elements mdb-news business logic added

--- src/app/mdb-news/mdb-news.component.ts
+++ src/app/mdb-news/mdb-news.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { News } from '../../tools/interfaces/news';
+import { FireService } from '../../tools/services/fire.service';
 
 @Component({
   selector: 'app-mdb-news',
   templateUrl: './mdb-news.component.html',
-  styleUrls: ['./mdb-news.component.css']
+  styleUrls: ['./mdb-news.component.css'],
 })
 export class MdbNewsComponent implements OnInit {
+  latestNewsElement: News;
+  news: News[];
 
-  constructor() { }
+  constructor(private db: FireService) {}
 
   ngOnInit() {
+    this.db.NewsControls.getNewsElements().subscribe(res => {
+      this.news = Object.values(res).sort((a: News, b: News) => b.timeStamp - a.timeStamp);
+      this.latestNewsElement = this.news.shift();
+    });
   }
-
 }
