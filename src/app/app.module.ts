55d2587272d3a96e0f534526d49919f795cb53c8
vipomage <vipomage@gmail.com>
2019-03-07 19:27:24 +0200

    Admin Module Split from App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // noinspection ES6UnusedImports
-import { BrowserModule, enableDebugTools } from '@angular/platform-browser';
-import { keyValueFilterPipe } from '../tools/pipes/key-value-filter.pipe';
-import { objectSplitToArray } from '../tools/pipes/split-array.pipe';
-import { ArraySortPipe} from '../tools/pipes/array-sort.pipe';
+import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { FireService } from '../tools/services/fire.service';
+import { FireService } from './tools/services/fire.service';
 import { environment } from '../environments/environment';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
@@ -17,27 +14,9 @@ import { AngularFireModule } from 'angularfire2';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { TableModule } from 'primeng/table';
-
 import { NgModule } from '@angular/core';
-import { ProjectsControlComponent } from './admin/controls/projects/projects-control/projects-control.component';
-import { PloughControlsComponent } from './admin/controls/ploughs/plough-controls/plough-controls.component';
-import { RemoveProjectsComponent } from './admin/controls/projects/remove-projects/remove-projects.component';
-import { AwardsControlsComponent } from './admin/controls/awards/awards-controls/awards-controls.component';
-import { EditProjectsComponent } from './admin/controls/projects/edit-projects/edit-projects.component';
-import { RemoveAwardsComponent } from './admin/controls/awards/remove-awards/remove-awards.component';
-import { AddProjectsComponent } from './admin/controls/projects/add-projects/add-projects.component';
-import { EditPloughsComponent } from './admin/controls/ploughs/edit-ploughs/edit-ploughs.component';
-import { AddCategoryComponent } from './admin/controls/ploughs/add-category/add-category.component';
-import { HomeControlsComponent } from './admin/controls/home/home-controls/home-controls.component';
-import { NewsControlComponent } from './admin/controls/news/news-control/news-control.component';
-import { EditAwardsComponent } from './admin/controls/awards/edit-awards/edit-awards.component';
-import { AddPloughComponent } from './admin/controls/ploughs/add-plough/add-plough.component';
-import { AddAwardsComponent } from './admin/controls/awards/add-awards/add-awards.component';
+
 import { AccordionComponent } from './products/accordion_DEPRECATED/accordion.component';
-import { EditHomeComponent } from './admin/controls/home/edit-home/edit-home.component';
-import { EditNewsComponent } from './admin/controls/news/edit-news/edit-news.component';
-import { AddNewsComponent } from './admin/controls/news/add-news/add-news.component';
-import { AddHomeComponent } from './admin/controls/home/add-home/add-home.component';
 import { MdbAccordionComponent } from './products/mdb-accordion/mdb-accordion.component';
 import { ProductTableComponent } from './products/product-table/product-table.component';
 import { CultivatorsComponent } from './products/cultivators/cultivators.component';
@@ -47,7 +26,6 @@ import { MdbCardComponent } from './mdb-card_DEPRECATED/mdb-card.component';
 import { MdbJumbotronComponent } from './mdb-jumbotron/mdb-jumbotron.component';
 import { CategoryComponent } from './products/category/category.component';
 import { MdbCarouselComponent } from './mdb-carousel/mdb-carousel.component';
-import { MdbTableComponent } from './admin/mdb-table/mdb-table.component';
 import { MdbSectionComponent } from './mdb-section/mdb-section.component';
 import { ProductComponent } from './products/product/product.component';
 import { PloughsComponent } from './products/ploughs/ploughs.component';
@@ -67,56 +45,35 @@ import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AwardsComponent } from './awards/awards.component';
 import { AboutComponent } from './about/about.component';
-import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 import { routes } from './routes';
+import { PipesModule } from './tools/pipes/pipes.module';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
-    ProjectsControlComponent,
-    PloughControlsComponent,
-    AwardsControlsComponent,
-    RemoveProjectsComponent,
     MdbAccordionComponent,
     ProductTableComponent,
-    RemoveAwardsComponent,
     MdbJumbotronComponent,
-    EditProjectsComponent,
-    HomeControlsComponent,
-    EditPloughsComponent,
-    AddCategoryComponent,
     CultivatorsComponent,
-    NewsControlComponent,
     SubcategoryComponent,
     MdbCarouselComponent,
-    AddProjectsComponent,
     MdbSectionComponent,
-    EditAwardsComponent,
-    keyValueFilterPipe,
-    objectSplitToArray,
-    ArraySortPipe,
     ShreddersComponent,
     AccordionComponent,
-    AddAwardsComponent,
-    AddPloughComponent,
     MdbFooterComponent,
-    EditNewsComponent,
     ProjectsComponent,
     ProductsComponent,
     CategoryComponent,
-    MdbTableComponent,
-    EditHomeComponent,
     ProductComponent,
     MdbCardComponent,
-    AddNewsComponent,
     PloughsComponent,
     RollersComponent,
     SpecialComponent,
     RippersComponent,
     HistoryComponent,
     MdbNewsComponent,
-    AddHomeComponent,
     FooterComponent,
     HeaderComponent,
     NavbarComponent,
@@ -125,7 +82,6 @@ import { routes } from './routes';
     PartsComponent,
     ToolsComponent,
     AboutComponent,
-    AdminComponent,
     HomeComponent,
     AppComponent,
   ],
@@ -133,6 +89,7 @@ import { routes } from './routes';
     RouterModule.forRoot(routes, { enableTracing: false }),
     AngularFireModule.initializeApp(environment.firebase),
     MDBBootstrapModule.forRoot(),
+    AdminModule,
     AngularFireDatabaseModule,
     AngularFireStorageModule,
     BrowserAnimationsModule,
@@ -143,8 +100,11 @@ import { routes } from './routes';
     BrowserModule,
     TableModule,
     FormsModule,
+    PipesModule,
   ],
+
   bootstrap: [AppComponent],
   providers: [FireService],
+  exports:[RouterModule]
 })
 export class AppModule {}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -17,25 +17,6 @@ import { AboutComponent } from './about/about.component';
 import { HistoryComponent } from './history/history.component';
 import { AwardsComponent } from './awards/awards.component';
 import { ProductComponent } from './products/product/product.component';
-import { AdminComponent } from './admin/admin.component';
-import { PloughControlsComponent } from './admin/controls/ploughs/plough-controls/plough-controls.component';
-import { EditPloughsComponent } from './admin/controls/ploughs/edit-ploughs/edit-ploughs.component';
-import { AddPloughComponent } from './admin/controls/ploughs/add-plough/add-plough.component';
-import { AddCategoryComponent } from './admin/controls/ploughs/add-category/add-category.component';
-import { AddNewsComponent } from './admin/controls/news/add-news/add-news.component';
-import { AddAwardsComponent } from './admin/controls/awards/add-awards/add-awards.component';
-import { AddProjectsComponent } from './admin/controls/projects/add-projects/add-projects.component';
-import { AwardsControlsComponent } from './admin/controls/awards/awards-controls/awards-controls.component';
-import { EditAwardsComponent } from './admin/controls/awards/edit-awards/edit-awards.component';
-import { RemoveAwardsComponent } from './admin/controls/awards/remove-awards/remove-awards.component';
-import { NewsControlComponent } from './admin/controls/news/news-control/news-control.component';
-import { EditNewsComponent } from './admin/controls/news/edit-news/edit-news.component';
-import { ProjectsControlComponent } from './admin/controls/projects/projects-control/projects-control.component';
-import { EditProjectsComponent } from './admin/controls/projects/edit-projects/edit-projects.component';
-import { RemoveProjectsComponent } from './admin/controls/projects/remove-projects/remove-projects.component';
-import { HomeControlsComponent } from './admin/controls/home/home-controls/home-controls.component';
-import { AddHomeComponent } from './admin/controls/home/add-home/add-home.component';
-import { EditHomeComponent } from './admin/controls/home/edit-home/edit-home.component';
 
 export const routes: Routes = [
   {
@@ -44,11 +25,7 @@ export const routes: Routes = [
     children: [
       { path: '', component: HomeComponent, outlet: 'products' },
       { path: 'rippers', component: RippersComponent, outlet: 'products' },
-      {
-        path: 'cultivators',
-        component: CultivatorsComponent,
-        outlet: 'products',
-      },
+      { path: 'cultivators',component: CultivatorsComponent,outlet: 'products'},
       { path: 'shredders', component: ShreddersComponent, outlet: 'products' },
       { path: 'ploughs', component: PloughsComponent, outlet: 'products' },
       { path: 'special', component: SpecialComponent, outlet: 'products' },
@@ -58,89 +35,13 @@ export const routes: Routes = [
       { path: 'tools', component: ToolsComponent, outlet: 'products' },
     ],
   },
-  {
-    path: 'details/:type/:category/:subCategory',
-    component: SubcategoryComponent,
-  },
+  { path: 'details/:type/:category/:subCategory', component: SubcategoryComponent},
   { path: 'projects', component: ProjectsComponent },
   { path: 'home', component: HomeComponent },
   { path: 'news', component: MdbNewsComponent },
   { path: 'about', component: AboutComponent },
   { path: 'history', component: HistoryComponent },
   { path: 'awards', component: AwardsComponent },
-  {
-    path: 'product/:type/:category/:subCategory/:id',
-    component: ProductComponent,
-  },
-  {
-    path: 'admin',
-    component: AdminComponent,
-    children: [
-      {
-        path: 'ploughs',
-        component: PloughControlsComponent,
-        outlet: 'admin',
-        children: [
-          { path: 'edit', component: EditPloughsComponent, outlet: 'controls' },
-          { path: 'add', component: AddPloughComponent, outlet: 'controls' },
-          {
-            path: 'addCategory',
-            component: AddCategoryComponent,
-            outlet: 'controls',
-          },
-        ],
-      },
-      {
-        path: 'home',
-        component: HomeControlsComponent,
-        outlet: 'admin',
-        children: [
-          { path: 'add', component: AddHomeComponent, outlet: 'controls' },
-          { path: 'edit', component: EditHomeComponent, outlet: 'controls' },
-        ],
-      },
-      {
-        path: 'awards',
-        component: AwardsControlsComponent,
-        outlet: 'admin',
-        children: [
-          { path: 'add', component: AddAwardsComponent, outlet: 'controls' },
-          { path: 'edit', component: EditAwardsComponent, outlet: 'controls' },
-          {
-            path: 'remove',
-            component: RemoveAwardsComponent,
-            outlet: 'controls',
-          },
-        ],
-      },
-      {
-        path: 'news',
-        component: NewsControlComponent,
-        outlet: 'admin',
-        children: [
-          { path: 'add', component: AddNewsComponent, outlet: 'controls' },
-          { path: 'edit', component: EditNewsComponent, outlet: 'controls' },
-        ],
-      },
-      {
-        path: 'projects',
-        component: ProjectsControlComponent,
-        outlet: 'admin',
-        children: [
-          { path: 'add', component: AddProjectsComponent, outlet: 'controls' },
-          {
-            path: 'edit',
-            component: EditProjectsComponent,
-            outlet: 'controls',
-          },
-          {
-            path: 'remove',
-            component: RemoveProjectsComponent,
-            outlet: 'controls',
-          },
-        ],
-      },
-    ],
-  },
-  { path: '**', redirectTo: '/home' },
+  { path: 'product/:type/:category/:subCategory/:id',component: ProductComponent},
+  { path: '', redirectTo: 'home',pathMatch:'full' },
 ];
