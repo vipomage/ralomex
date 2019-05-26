fcc3814aeb29b199c8839b2ee5df30aa36e96a74
Eray <vipomage@gmail.com>
2019-05-26 19:55:12 +0300

    Project default Styles set to SCSS Products component Update (halfway) Prettier formatter config update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  public visible: boolean = true;
+  public visible = true;
   constructor() {}
 
   itemPicked = () => (this.visible = !this.visible);
